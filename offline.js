921f410523c4fb969ca821bd97a7e832dd6c8c64
DESKTOP-BVB85B5\zhang <805024273@qq.com>
2017-04-28 15:41:04 +0800

    小飞机

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1493362158,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/background.png",
+		"images/xiafeiji-sheet0.png",
+		"images/xiafeiji-sheet1.png",
+		"images/zidan-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/bianxing-sheet0.png",
+		"images/bianxing-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
